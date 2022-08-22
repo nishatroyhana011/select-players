132f7f2506a07ef198f00dddabd6b1b04e6ef57f
nishat <nishatroyhana011@gmail.com>
2022-08-22 12:44:49 +0600

    player fee calculation added

--- script.js
+++ script.js
@@ -7,20 +7,32 @@ function disabledButton(btn){
     btn.style.backgroundColor = 'gray';
     btn.style.cursor = 'no-drop';
 }
+
+//player selection
 for(let button of selectButtons){
     button.addEventListener('click', function(event){
 
         let playerName = event.target.parentNode.children[0].innerText;
-        playersArray.push(playerName);
 
-        if( playersArray.length <= 5 ){
+        if( playersArray.length < 5 ){           
+            playersArray.push(playerName);
             let player = document.createElement('li');
             player.innerText = playerName;
             players.appendChild(player);
             disabledButton(event.target);
         }else{
             alert('You have selected 5 players.');
-        }
-        
+        }   
     })
 }
+
+//player fee calculation
+document.getElementById('player-fee-total').addEventListener('click', function(){
+
+    const playerFee = parseInt(document.getElementById('player-fee').value);
+    let playersFeeTotal = playersArray.length * playerFee;
+    document.getElementById('player-expense').innerText = playersFeeTotal;
+})
+
+
+
